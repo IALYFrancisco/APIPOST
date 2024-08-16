88b3b7d81c7182d7c5e9dc878e52d110da17c064
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2024-08-16 06:43:49 +0300

    reglage de liens de requests.ts

--- src/stores/requests.ts
+++ src/stores/requests.ts
@@ -10,32 +10,32 @@ export const useRequestStore: any = defineStore('requestStore', {
       pythonRequestLaunched: false
     }
   },
-  // getters: {
-  //   async getJavascriptData() {
-  //     await axios.get('https://apipostserver.onrender.com/product/9').then(async (response: any) => {
-  //       this.javascriptData = await response.data
-  //     })
-  //     this.javascriptRequestLaunched = true
-  //   },
-  //   async getPythonData() {
-  //     await axios.get('https://apipostserver.onrender.com/product/9').then(async (response: any) => {
-  //       this.pythonData = await response.data
-  //     })
-  //     this.pythonRequestLaunched = true
-  //   }
-  // }
   getters: {
     async getJavascriptData() {
-      await axios.get('http://127.0.0.1:3000/product/9').then(async (response: any) => {
+      await axios.get('https://apipostserver.onrender.com/product/9').then(async (response: any) => {
         this.javascriptData = await response.data
       })
       this.javascriptRequestLaunched = true
     },
     async getPythonData() {
-      await axios.get('http://127.0.0.1:3000/product/9').then(async (response: any) => {
+      await axios.get('https://apipostserver.onrender.com/product/9').then(async (response: any) => {
         this.pythonData = await response.data
       })
       this.pythonRequestLaunched = true
     }
   }
+  // getters: {
+  //   async getJavascriptData() {
+  //     await axios.get('http://127.0.0.1:3000/product/9').then(async (response: any) => {
+  //       this.javascriptData = await response.data
+  //     })
+  //     this.javascriptRequestLaunched = true
+  //   },
+  //   async getPythonData() {
+  //     await axios.get('http://127.0.0.1:3000/product/9').then(async (response: any) => {
+  //       this.pythonData = await response.data
+  //     })
+  //     this.pythonRequestLaunched = true
+  //   }
+  // }
 })
